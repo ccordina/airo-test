7fb24c1d06156d95539d0f73fa61bbb95b2dc8fb
ccordina <ccordina@gmail.com>
2026-06-15 11:59:13 +0200

    Add migration for quotations table to store trip details and costs

---
+++ database/migrations/2026_06_13_093927_add_quotation_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quotations', function (Blueprint $table) {
+            $table->id();
+            $table->string('age');
+            $table->string('currency_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('trip_length');
+            $table->decimal('total_cost', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quotations');
+    }
+};
